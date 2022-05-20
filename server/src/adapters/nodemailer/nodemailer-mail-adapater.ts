b122b40e21ac30f60371d30f4860bc5c9e966f2c
joiceramos <joice.ramos@sevensys.tech>
2022-05-19 22:26:16 -0300

    first commit

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapater.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from '../mail-adapter';
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "290f3d353b4a4f",
+    pass: "f17ae36dd3552c"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com',
+      to: 'Joice Ramos <joice_ramos@outlook.com.br',
+      subject,
+      html: body,
+    })
+  };
+};
